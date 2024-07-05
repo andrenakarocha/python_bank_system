31035d4774b30f28e346ec8bf6a72b96b838303c
Andre Rocha <andrenakarocha@hotmail.com>
2024-07-05 15:30:44 -0300

    Projeto V3

--- Bank System.py
+++ Bank System.py
@@ -80,7 +80,7 @@ class Conta:
         return True
 
 class ContaCorrente(Conta):
-    def __init__(self, numero, cliente, limite, limite_saques):
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
         self.limite = limite
         self.limite_saques = limite_saques
         super().__init__(numero, cliente)
@@ -97,11 +97,14 @@ class ContaCorrente(Conta):
         elif excedeu_saques:
             print("Operação falhou! Número máximo de saques excedido.")
 
+        else:
+            return super().sacar(valor)
+
     def __str__(self):
         return f"""
                 Agência:\t{self.agencia}
                 Número Conta:\t{self.numero}
-                Titular:\n{self.cliente.nome}
+                Titular:\t{self.cliente.nome}
         """
 
 class Historico:
@@ -159,20 +162,126 @@ class Deposito(Transacao):
         if sucesso_transacao:
             conta.historico.adicionar_transacao(self)
 
-menu = '''
-Welcome to Awesome Bank! What would you like to do?
+def depositar(clientes):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
 
-    ------Transactions------
-    [1] - Deposit
-    [2] - Withdraw
-    [3] - Check Extract
-    [4] - Leave
+    if not cliente:
+        print("\nCliente não encontrado!")
+        return
+    
+    valor = float(input("Informe o valor do depósito: "))
+    transacao = Deposito(valor)
+
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    cliente.realizar_transacao(conta, transacao)
+
+def sacar(clientes):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+
+    if not cliente:
+        print("\nCliente não encontrado!")
+        return
+    
+    valor = float(input("Informe o valor do saque: "))
+    transacao = Saque(valor)
+
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    cliente.realizar_transacao(conta, transacao)
+
+def exibir_extrato(clientes):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+
+    if not cliente:
+        print("\nCliente não encontrado!")
+        return
+    
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    print("\nEXTRATO:")
+    transacoes =  conta.historico.transacoes
+
+    extrato = ""
+    if not transacoes:
+        extrato = "Não foram realizadas transações."
+
+    else:
+        for transacao in transacoes:
+            extrato += f"\n{transacao['tipo']}: \n\tR${transacao['valor']:.2f}"
+
+    print(extrato)
+
+def criar_conta(numero_conta, clientes, contas):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+
+    if not cliente:
+        print("Cliente não encontrado! Criação de conta encerrada!")
+        return
+    
+    conta = ContaCorrente.nova_conta(cliente=cliente, numero=numero_conta)
+    contas.append(conta)
+    cliente.contas.append(conta)
+
+    print("\nConta criada com sucesso!")
+
+def criar_cliente(clientes):
+    cpf = input("Informe o CPF (somente número): ")
+    cliente = filtrar_cliente(cpf, clientes)
+
+    if cliente:
+        print("\nJá existe cliente com esse CPF!")
+        return
+    
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento: ")
+    endereco = input("Informe o endereço: ")
+
+    cliente = PessoaFisica(endereco, nome, data_nascimento, cpf)
+
+    clientes.append(cliente)
+
+    print("\nCliente criado com sucesso!")
+
+def filtrar_cliente(cpf, clientes):
+    clientes_filtrados = [cliente for cliente in clientes if cliente.cpf == cpf]
+    return clientes_filtrados[0] if clientes_filtrados else None
+
+def recuperar_conta_cliente(cliente):
+    if not cliente.contas:
+        print("\nCliente não possui conta!")
+        return
+    
+    return cliente.contas[0]
+
+def listar_contas(contas):
+    for conta in contas:
+        print(str(conta))
+
+menu = '''
+Bem vindo ao Banco Incrível, o que deseja fazer?
+
+    -------Transações-------
+    [1] - Depositar
+    [2] - Sacar
+    [3] - Checar Extrato
+    [4] - Sair
     ------------------------
 
     ----------User----------
-    [5] - Create User
-    [6] - Create Account
-    [7] - List Accounts
+    [5] - Criar Usuário
+    [6] - Criar Conta
+    [7] - Listar Contas
     ------------------------
 '''
 
@@ -184,25 +293,28 @@ def main():
         opcao = input(menu)
 
         if opcao == "1":
-            pass
+            depositar(clientes)
 
         elif opcao == "2":
-            pass
+            sacar(clientes)
 
         elif opcao == "3":
-            pass
+            exibir_extrato(clientes)
         
         elif opcao == "4":
             break
 
         elif opcao == "5":
-            pass
+            criar_cliente(clientes)
 
         elif opcao == "6":
-            pass
+            numero_conta = len(contas) + 1
+            criar_conta(numero_conta, clientes, contas)
 
         elif opcao == "7":
-            pass
+            listar_contas(contas)
 
         else:
             print("Essa não é uma opção válida!")
+
+main()
